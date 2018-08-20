71a5281f8bc161c560fdc6ab877564d563bbfb01
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-08-20 12:07:44 +0700

    Add database

---
+++ configDB/sequelize.js
@@ -0,0 +1,27 @@
+'use strict';
+const config = require('./config');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.server, config.username, config.password, {
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
+        timestamps: true
+    },
+});
+sequelize.sync()
+const op = Sequelize.Op;
+
+const model = {};
+model.Sequelize = Sequelize;
+model.sequelize = sequelize;
+model.op = op;
+
+model.food =  require('../models/foods')(sequelize, Sequelize);
+// model.cart =  require('../models/cart')(sequelize, Sequelize);
+// model.product =  require('../models/product')(sequelize, Sequelize);
+// model.project =  require('../models/project')(sequelize, Sequelize);
+// model.users=  require('../models/user')(sequelize, Sequelize);
+
+// model.cart.belongsTo(model.product);
+// model.product.hasMany(model.cart); // take userId to reseration
+module.exports = model;
