aa0521f834893c2202622b17b79cea372dc0bb5a
tar <tar@sandboxglobal.com>
2018-09-16 19:28:28 +0700

    update pic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+const helmet = require('helmet');
+var compression = require('compression');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -16,6 +18,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', require('express-ejs-extend'));
 app.set('view engine', 'ejs');
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
